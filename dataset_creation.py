35584d77460b1e7cb534ca79bb3b2619f397d9aa
Sai Anirud <saianirud>
2021-11-11 19:02:53 -0700

    added dataset and finetuner code

--- dataset_creation.py
+++ dataset_creation.py
@@ -1,75 +1,64 @@
-import json
+import jsonlines
 import random
-from enum import Enum, unique
+import os
 
-@unique
-class NumbersTypes(Enum):
-    '''
-        Enum for three types of numbers list containing - 1) Only Positive 2) Only Negative 3) Both Positive and Negative
-    '''
-    POSITIVE = 1
-    NEGATIVE = 2
-    BOTH = 3
+for no_of_digits in [2, 3, 4]:
 
+    # length of dataset
+    dataset_length = 100000
 
-dataset = []
+    # minimum and maximum length of list
+    min_no_of_numbers = 3
+    max_no_of_numbers = 11
 
-# length of dataset
-dataset_length = 1002
-
-# minimum and maximum length of list
-min_no_of_numbers = 5
-max_no_of_numbers = 10
-
-# range of numbers in the list
-min_number = -100
-max_number = 100
-
-# alter this list accordingly for different types
-numbers_types = [NumbersTypes.POSITIVE, NumbersTypes.NEGATIVE, NumbersTypes.BOTH]
-
-# get the range of each numbers type
-numbers_types_range = []
-for numbers_type in numbers_types:
-    if numbers_type == NumbersTypes.POSITIVE and max_number > 0:
-        numbers_types_range.append([0, max_number])
-    elif numbers_type == NumbersTypes.NEGATIVE and min_number < 0:
-        numbers_types_range.append([min_number, 0])
-    elif numbers_type == NumbersTypes.BOTH:
-        numbers_types_range.append([min_number, max_number])
-
-
-for each_range in numbers_types_range:
-    # for ascending or descending order
-    is_asc = True
+    # range of numbers in the list
+    min_number = 0
+    if no_of_digits == 2:
+        max_number = 100
+        path = 'dataset_pretrain/sort_99'
+    elif no_of_digits == 3:
+        max_number = 1000
+        path = 'dataset_pretrain/sort_999'
+    elif no_of_digits == 4:
+        max_number = 10000
+        path = 'dataset_pretrain/sort_9999'
     
-    for i in range(dataset_length//len(numbers_types_range)):
-        # get a random length of list between minimum and maximum
-        no_of_numbers = random.choice(range(min_no_of_numbers, max_no_of_numbers))
 
-        # generate numbers list according to its range
-        numbers = random.sample(range(each_range[0], each_range[1]), no_of_numbers)
-        
-        random.shuffle(numbers)
-        # get the sorted order
-        sorted_numbers = sorted(numbers) if is_asc else sorted(numbers, reverse=True)
-        
-        # form the input string in the following format
-        numbers_string = ', '.join(str(x) for x in numbers)
-        sorted_numbers_string = ', '.join(str(x) for x in sorted_numbers)
-        asc_desc = 'ascending' if is_asc else 'descending'
-        sample = 'The sorted {0} order of {1} is {2}'.format(asc_desc, numbers_string, sorted_numbers_string)
-        
-        dataset.append({
-            'input': numbers,
-            'output': sorted_numbers,
-            'sample': sample,
-            'asc': is_asc
-        })
-        is_asc = not is_asc
+    if not os.path.exists(path):
+        os.makedirs(path)
 
-random.shuffle(dataset)
-  
-# write the generated dataset to a file
-with open('dataset_pretrain.json', 'w+') as f:
-    json.dump(dataset, f, indent=4)
+    for file in ['train', 'val', 'test']:
+
+        dataset = []
+            
+        for i in range(dataset_length):
+            # get a random length of list between minimum and maximum
+            no_of_numbers = random.choice(range(min_no_of_numbers, max_no_of_numbers))
+
+            # generate numbers list according to its range
+            numbers = random.sample(range(min_number, max_number), no_of_numbers)
+            
+            random.shuffle(numbers)
+            
+            # get the sorted order
+            sorted_numbers = sorted(numbers)
+            numbers_string = ' '.join(str(x) for x in numbers)
+            sorted_numbers_string = ' '.join(str(x) for x in sorted_numbers)
+            asc_text = 'The sorted ascending order of {0} is {1}'.format(numbers_string, sorted_numbers_string)
+
+            rev_sorted_numbers = sorted(numbers, reverse=True)
+            numbers_string = ' '.join(str(x) for x in numbers)
+            sorted_numbers_string = ' '.join(str(x) for x in rev_sorted_numbers)
+            desc_text = 'The sorted descending order of {0} is {1}'.format(numbers_string, sorted_numbers_string)
+            
+            dataset.append({
+                'numbers': numbers,
+                'asc_text': asc_text,
+                'desc_text': desc_text
+            })
+
+        random.shuffle(dataset)
+        
+        # write the generated dataset to a file
+        with jsonlines.open(path + '/' + file + '.jsonl', 'w') as writer:
+            writer.write_all(dataset)
---
+++ utils.py
@@ -0,0 +1,172 @@
+import pytorch_lightning as pl
+import torch
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+from typing import List
+
+
+def compute_exact_match(predicted_answer, correct_answer) -> bool:
+    predicted_answer = predicted_answer.strip().lower().replace(" ","")
+    correct_answer = correct_answer.strip().lower().replace(" ","")
+    return predicted_answer == correct_answer
+
+
+class T5(pl.LightningModule):
+
+    def __init__(self, hparams, train_dataloader, val_dataloader, test_dataloader):
+        super(T5, self).__init__()
+
+        self.hparams = hparams
+
+        self.tokenizer = AutoTokenizer.from_pretrained(self.hparams.model_name_or_path)
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(self.hparams.model_name_or_path)
+
+        self._train_dataloader = train_dataloader
+        self._val_dataloader = val_dataloader
+        self._test_dataloader = test_dataloader
+
+    def prepare_batch(self, questions: List[str], answers: List[str]) -> List[str]:
+
+        input_dict = self.tokenizer.batch_encode_plus(
+            list(questions), padding=True, truncation=False, return_tensors='pt')
+
+        labels = self.tokenizer.batch_encode_plus(
+            list(answers), padding=True, truncation=False, return_tensors='pt')['input_ids']
+
+        assert input_dict['input_ids'].shape[1] < self.hparams.max_seq_length
+        assert labels.shape[1] < self.hparams.max_seq_length
+
+        input_ids = input_dict['input_ids'].to(self.model.device)
+        attention_mask = input_dict['attention_mask'].to(self.model.device)
+        labels = labels.to(self.model.device)
+
+        return input_ids, attention_mask, labels
+
+    def forward(self, **kwargs):
+        return self.model(**kwargs)
+
+    def training_step(self, batch, batch_nb):
+        questions, correct_answers = batch
+
+        # Log every power of two.
+        if batch_nb & (batch_nb - 1) == 0:
+            print(questions[0])
+            print(correct_answers[0])
+
+        input_ids, attention_mask, labels = self.prepare_batch(
+            questions=questions, answers=correct_answers)
+
+        loss = self.model(input_ids=input_ids,
+                          attention_mask=attention_mask,
+                          labels=labels)[0]
+
+        tensorboard_logs = {'train_loss': loss}
+        return {'loss': loss, 'log': tensorboard_logs}
+
+    def inference_step(self, batch, batch_nb: int):
+        questions, correct_answers = batch
+
+        input_ids, attention_mask, _ = self.prepare_batch(
+            questions=questions, answers=correct_answers)
+
+        batch_outputs = self.model.generate(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            do_sample=False,
+            max_length=self.hparams.max_seq_length)
+
+        predicted_answers = [
+            self.tokenizer.decode(output, skip_special_tokens=True, clean_up_tokenization_spaces=True)
+            for output in batch_outputs]
+
+        exact_matches = [
+            compute_exact_match(predicted_answer=predicted_answer, correct_answer=correct_answer)
+            for predicted_answer, correct_answer in zip(predicted_answers, correct_answers)]
+
+        # Log every power of two.
+        if batch_nb & (batch_nb - 1) == 0:
+            print('\nQuestion:', questions[0])
+            print('Correct:  ', correct_answers[0])
+            print('Predicted:', predicted_answers[0].encode('utf-8'))
+            print('Exact?', exact_matches[0])
+
+        metrics = {'exact_matches': exact_matches}
+        return metrics
+
+    def validation_step(self, batch, batch_nb):
+        return self.inference_step(batch, batch_nb)
+
+    def test_step(self, batch, batch_nb):
+        return self.inference_step(batch, batch_nb)
+
+    def validation_epoch_end(self, outputs):
+        exact_matches = []
+        for x in outputs:
+            exact_matches.extend(x['exact_matches'])
+        exact_match = sum(exact_matches) / len(exact_matches)
+
+        metrics = {'val_exact_match': exact_match}
+
+        output = metrics.copy()
+        output['progress_bar'] = metrics
+
+        return output
+
+    def test_epoch_end(self, outputs):
+        exact_matches = []
+        for x in outputs:
+            exact_matches.extend(x['exact_matches'])
+        exact_match = sum(exact_matches) / len(exact_matches)
+
+        metrics = {'test_exact_match': exact_match}
+
+        output = metrics.copy()
+        output['progress_bar'] = metrics
+
+        return output
+
+    def train_dataloader(self):
+        return self._train_dataloader
+
+    def val_dataloader(self):
+        return self._val_dataloader
+
+    def test_dataloader(self):
+        return self._test_dataloader
+
+    def get_optimizer(self):
+        optimizer_name = self.hparams.optimizer
+        scheduler_name = self.hparams.scheduler
+        lr = self.hparams.lr
+        weight_decay = self.hparams.weight_decay
+
+        optimizer = getattr(torch.optim, optimizer_name)
+
+        # Prepare optimizer and schedule (linear warmup and decay)
+        no_decay = ["bias", "LayerNorm.weight"]
+        optimizer_grouped_parameters = [
+            {
+                "params": [p for n, p in self.model.named_parameters() if not any(nd in n for nd in no_decay)],
+                "weight_decay": weight_decay,
+            },
+            {
+                "params": [p for n, p in self.model.named_parameters() if any(nd in n for nd in no_decay)],
+                "weight_decay": 0.0
+            },
+        ]
+        optimizer = optimizer(optimizer_grouped_parameters, lr=lr, weight_decay=weight_decay)
+
+        print(f'=> Using {optimizer_name} optimizer')
+
+        if scheduler_name == 'StepLR':
+            scheduler = torch.optim.lr_scheduler.StepLR(
+                optimizer, step_size=self.hparams.step_size, gamma=self.hparams.gamma)
+            print(f'=> Using StepLR (step_size = {self.hparams.step_size}, gamma = {self.hparams.gamma})')
+        else:
+            raise Exception(f'Scheduler not implemented: {scheduler_name}')
+
+        return [optimizer], [scheduler]
+
+    def configure_optimizers(self):
+        optimizer = self.get_optimizer()
+        return optimizer

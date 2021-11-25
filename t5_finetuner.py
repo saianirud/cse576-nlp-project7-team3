826fbc01c857792ae2e28e1a54c28cc42e05076c
Sai Anirud <saianirud>
2021-11-24 20:19:06 -0700

    monitor metric

--- t5_finetuner.py
+++ t5_finetuner.py
@@ -122,8 +122,8 @@ if __name__ == '__main__':
     test_dataloader_ood = DataLoader(dataset_test_ood, batch_size=args.val_batch_size, shuffle=False, num_workers=args.num_workers)
 
     checkpoint_callback = ModelCheckpoint(
-        filepath=os.path.join(args.output_dir, args.model_prefix + '-{epoch}-{val_loss:.4f}'),
-        verbose=False, save_last=False, save_top_k=1, mode='min', monitor='val_loss',
+        filepath=os.path.join(args.output_dir, args.model_prefix + '-{epoch}-{val_exact_match:.4f}'),
+        verbose=False, save_last=False, save_top_k=1, mode='max', monitor='val_exact_match',
         save_weights_only=False, period=args.check_val_every_n_epoch)
 
     trainer = pl.Trainer.from_argparse_args(args, checkpoint_callback=checkpoint_callback)
